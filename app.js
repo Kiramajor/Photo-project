fb0d8b45778a99677d7db91152a7f1ec10d6124f
Kira Major <majorbunene@yahoo.com>
2024-03-24 18:25:32 +0100

    restructured js and added more styling

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
 const submitBtn = document.querySelector(".submit-btn");
 const form = document.querySelector(".search-form");
+const more = document.querySelector(".more");
 let searchValue;
+let page = 1;
+let fetchLink;
+let currentSearch;
 
 // Event Listeners
 searchInput.addEventListener("input", updateInput);
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  currentSearch = searchValue;
   searchPhotos(searchValue);
 });
+more.addEventListener("click", loadMore);
 
 // Functions
 function updateInput(e) {
@@ -44,22 +50,31 @@ function generatePictures(data) {
 }
 
 async function curatedPhotos() {
-  const data = await fetchApi(
-    "https://api.pexels.com/v1/curated?per_page=15&page=1"
-  );
+  fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1";
+  const data = await fetchApi(fetchLink);
   generatePictures(data);
 }
 
 async function searchPhotos(search) {
   clear();
-  const data = await fetchApi(
-    `https://api.pexels.com/v1/search?query=${search}&per_page=15&page=1`
-  );
+  fetchLink = `https://api.pexels.com/v1/search?query=${search}&per_page=15&page=1`;
+  const data = await fetchApi(fetchLink);
   generatePictures(data);
 }
 
 function clear() {
   gallery.innerHTML = "";
   searchInput.value = "";
+}
+
+async function loadMore() {
+  page++;
+  if (currentSearch) {
+    fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${page}`;
+  } else {
+    fetchLink = `https://api.pexels.com/v1/curated?per_page=15&${page}`;
+  }
+  const data = await fetchApi(fetchLink);
+  generatePictures(data);
 }
 curatedPhotos();
